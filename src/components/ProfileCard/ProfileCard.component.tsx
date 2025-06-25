f1822bbdf419d06cec2a2cb57ae344d6e5fe8a2d
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-25 20:45:42 +0330

    feat: Add ProfileCard component

---
+++ src/components/ProfileCard/ProfileCard.component.tsx
@@ -0,0 +1,19 @@
+interface Props {
+  name: string;
+  image: string;
+  description: string;
+}
+
+export const ProfileCard = ({ name, image, description }: Props) => {
+  return (
+    <Card className="w-80">
+      <ImageLoader src={image} className="size-80" />
+
+      <div className="flex flex-col gap-2 p-3">
+        <h1 className="text-base font-bold tracking-tight text-gray-900 dark:text-white">{name}</h1>
+
+        <p className="text-sm text-gray-500 dark:text-gray-400">{description}</p>
+      </div>
+    </Card>
+  );
+};
