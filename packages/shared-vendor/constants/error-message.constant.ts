469658057a6dce973d0f639d55fa7670772916da
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-26 23:45:10 +0330

    chore: Change constants from js to ts

---
+++ packages/shared-vendor/constants/error-message.constant.ts
@@ -0,0 +1,1 @@
+export const CONTEXT_ERROR_MESSAGE = "Provider is not used or the hook is called outside of provider!";
--- packages/shared-vendor/constants/index.ts
+++ packages/shared-vendor/constants/index.ts
@@ -1,1 +1,2 @@
 export { default as MEDIA_QUERY } from "./media-query.constant";
+export * from "./error-message.constant";
