99a9973f5b2846dbaf3b475bff04cf0da04b3fa2
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-27 19:51:43 +0330

    fix: Fix NoTask size

--- src/components/ToDoList/Task/NoTask.component.tsx
+++ src/components/ToDoList/Task/NoTask.component.tsx
@@ -8,8 +8,8 @@ const DEFAULT_MESSAGE: Props["message"] = "کاری ندارید";
 
 const NoTask = ({ message = DEFAULT_MESSAGE }: Props) => {
   return (
-    <div className="flex size-full flex-col items-center justify-center gap-2 font-bold">
-      <DetectiveSvg className="size-40" />
+    <div className="flex size-full flex-col items-center justify-center gap-6 text-xl font-bold">
+      <DetectiveSvg />
 
       <span>{message}</span>
     </div>
