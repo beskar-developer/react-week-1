9a476d81c3afc6de52ebfe0c5c5ad8e84d9c2758
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-27 14:16:05 +0330

    feat: Add motion animation to TaskList

--- src/components/ToDoList/List/TaskList.component.tsx
+++ src/components/ToDoList/List/TaskList.component.tsx
@@ -2,21 +2,31 @@ import useTaskList from "./UseTaskList.hook";
 
 import { NoTask, Task as ToDoTask } from "@/components/ToDoList";
 
+const ANIMATION_CONFIG: MotionProps = {
+  layout: true,
+  initial: { opacity: 0, x: -400, scale: 0.5 },
+  animate: { opacity: 1, x: 0, scale: 1 },
+  exit: { opacity: 0, x: 200, scale: 1.2 },
+  transition: { duration: 0.6, type: "spring" },
+};
+
 const TaskList = () => {
   const { tasks, hasTasks } = useTaskList();
 
   return (
-    <>
+    <motion.ul>
       {hasTasks ? (
         tasks.map((task) => (
-          <div key={task.id}>
+          <motion.li {...ANIMATION_CONFIG} key={task.id}>
             <ToDoTask id={task.id} />
-          </div>
+          </motion.li>
         ))
       ) : (
-        <NoTask />
+        <motion.li {...ANIMATION_CONFIG}>
+          <NoTask />
+        </motion.li>
       )}
-    </>
+    </motion.ul>
   );
 };
 
