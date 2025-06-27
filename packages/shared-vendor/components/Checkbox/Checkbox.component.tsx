cb59a8fed9fc4465a020666497e195e8fc35d69b
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-27 19:49:21 +0330

    fix: Fix checkbox in light mode

--- packages/shared-vendor/components/Checkbox/Checkbox.component.tsx
+++ packages/shared-vendor/components/Checkbox/Checkbox.component.tsx
@@ -16,7 +16,7 @@ export const Checkbox = ({ value, label, disabled, strikeLabel, onValueChange }:
   return (
     <div className="flex items-center justify-center gap-1" onClick={toggleValue}>
       <div
-        className={`${BUTTON_CLASSES[state]} ${disabled ? "cursor-not-allowed opacity-55 grayscale-50" : ""} m-4 flex size-4.5 shrink-0 items-center justify-center rounded-sm border-2`}
+        className={`${BUTTON_CLASSES[state]} ${disabled ? "cursor-not-allowed opacity-55 grayscale-50" : ""} m-4 flex size-4.5 shrink-0 items-center justify-center rounded-sm border-2 text-white`}
       >
         <CheckTick state={state} />
       </div>
