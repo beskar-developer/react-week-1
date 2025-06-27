d208996e6a095413297b26482d4956c62bfb144f
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-27 12:16:03 +0330

    feat: Add Task component

---
+++ src/components/ToDoList/Task/Task.component.tsx
@@ -0,0 +1,23 @@
+import type { Props } from "./Task.type";
+
+import useTask from "./UseTask.hook";
+
+const Task = ({ id }: Props) => {
+  const { isSelected, task, actions, setTaskIsDone } = useTask(id);
+
+  return (
+    <div
+      className={`${isSelected ? "bg-indigo-50 dark:bg-gray-700" : ""} flex items-center justify-between p-1 text-sm font-semibold duration-200`}
+    >
+      <Checkbox strikeLabel value={task.isDone} label={task.text} onValueChange={setTaskIsDone} />
+
+      <div className="flex gap-2">
+        {actions.map(({ Component, name, ...props }) => (
+          <Component {...props} key={name} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Task;
---
+++ src/components/ToDoList/Task/UseTask.hook.ts
@@ -0,0 +1,33 @@
+import TrashSvg from "@/assets/svg/trash.svg?react";
+import PenSvg from "@/assets/svg/pen.svg?react";
+
+import type { Props } from "./Task.type";
+
+const useTask = (id: Props["id"]) => {
+  const { tasks, changeActionToEdit, deleteTask, selectedTaskId, setTaskIsDone } = useToDoListContext();
+
+  const task = findByKey(tasks, id);
+
+  const isSelected = id === selectedTaskId;
+
+  const wrapSetTaskIsDone = (value: boolean) => setTaskIsDone(id, value);
+
+  const actions = [
+    {
+      Component: PenSvg,
+      name: "edit",
+      className: "[&>*]:stroke-indigo-400",
+      onClick: () => changeActionToEdit(id),
+    },
+    {
+      Component: TrashSvg,
+      name: "delete",
+      className: "[&>*]:stroke-red-600",
+      onClick: () => deleteTask(id),
+    },
+  ];
+
+  return { task, isSelected, actions, setTaskIsDone: wrapSetTaskIsDone };
+};
+
+export default useTask;
