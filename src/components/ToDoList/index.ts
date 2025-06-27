dbfd629b4360253366d0d0740e7957f3155981c4
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-27 12:16:24 +0330

    feat: Add ToDoList components index facade

---
+++ src/components/ToDoList/index.ts
@@ -0,0 +1,4 @@
+export { default as Task } from "./Task/Task.component";
+export { default as TaskList } from "./List/TaskList.component";
+export { default as ActionForm } from "./Form/ActionForm.component";
+export { default as NoTask } from "./Task/NoTask.component";
