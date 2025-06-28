bac59950bb4e5f45f00fe493eea93e960edf1f04
AmirHossein Akhondzadeh <amirhossein.akhondzadeh.dev@gamil.com>
2025-06-28 16:32:05 +0330

    chore: Rename config files to ts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   formatter: "@commitlint/format",
-  ignores: [(commit) => commit === "", (commit) => commit.startsWith("Merge branch")],
+  ignores: [(commit: string) => commit === "", (commit: string) => commit.startsWith("Merge branch")],
   defaultIgnores: true,
   helpUrl: "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",
   rules: { "subject-case": [0] },
